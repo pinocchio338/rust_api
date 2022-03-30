96b2b8c079f401c46751c13d274497a9cb4deffc
willeslau <willeslau@gmail.com>
2022-03-30 18:59:03 +0800

    Willes/solana contract call rpc implementation (#9)
    * wip
    
    * compiles
    
    * working client interface:
    
    * add more comments
    
    * interface for other functions
    
    * cippy
    
    * remove unwanted code

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,8 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+mod error;
+mod datapoint;
+mod manager;
+
+pub use error::Error;
+pub use datapoint::DataPoint;
+pub use manager::Manager;
+
+pub type Bytes = Vec<u8>;
+pub type Bytes32 = [u8; 32];
+pub type Uint256 = [u8; 32];
